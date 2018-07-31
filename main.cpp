f0174dd15c921d67aeccf51d413c2036915c1107
kmerhoff <31228924+kmerhoff@users.noreply.github.com>
2018-07-30 21:50:07 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,15 +4,34 @@
 #include <csignal>
 #include <unistd.h>
 
-#include "u8g2_hal_rpi.h"
+#include <U8g2lib.h>
+#include <U8x8lib.h>
 
 using namespace std;
 
-u8g2_t u8g2;
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//GPIO Pins
+int d0_pin = 11;
+int d1_pin = 11;
+int d2_pin = 11;
+int d3_pin = 11;
+int d4_pin = 11;
+int d5_pin = 11;
+int d6_pin = 11;
+int d7_pin = 11;
+int enable_pin = 11;
+int cs_1_pin = 11;
+int dc_pin = 11;
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Screen init
+U8G2_T6963_128X64_F_8080 screen_1(U8G2_R0, d0_pin, d1_pin, d2_pin, d3_pin, d4_pin, d5_pin, d6_pin, d7_pin, enable_pin, cs_1_pin, dc_pin);
 
 static volatile bool complete = false;
 static int forced_exit_attempts = 0;
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Functions
 void exitHandler(int s) {
     if (forced_exit_attempts >= 2) {
         cout << "Forcing shutdown" << endl;
@@ -23,7 +42,7 @@ void exitHandler(int s) {
     forced_exit_attempts++;
 }
 
-void drawU8G2Logo(u8g2_t *u8g2) {
+void drawU8G2Logo(u8g2 *u8g2) {
     u8g2_ClearBuffer(u8g2);
     u8g2_SetFontMode(u8g2, 1);
 
@@ -47,7 +66,7 @@ void drawU8G2Logo(u8g2_t *u8g2) {
     u8g2_SendBuffer(u8g2);
 }
 
-void drawBannerText(u8g2_t *u8g2, uint32_t durationSecs) {
+void drawBannerText(u8g2 *u8g2, uint32_t durationSecs) {
 
     uint8_t x = 255;
     uint32_t prevMillis = 0;
@@ -78,8 +97,15 @@ void drawBannerText(u8g2_t *u8g2, uint32_t durationSecs) {
         delay(500);
     }
 }
-
-int main() {
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Setup
+/* u8g2.begin() is required and will sent the setup/init sequence to the display */
+void setup() {
+  screen_1.begin();
+}
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Main
+int loop() {
     struct sigaction sigIntHandler{};
     sigIntHandler.sa_handler = exitHandler;
     sigemptyset(&sigIntHandler.sa_mask);
@@ -89,32 +115,18 @@ int main() {
     //Initialize Wiring Pi
     wiringPiSetup();
 
-    //Configure the SPI pins
-    u8g2_rpi_hal_t u8g2_rpi_hal = {14 /*CLOCK/EN*/, 12 /*MOSI/RW*/, 10 /*CS/RS*/};
-    u8g2_rpi_hal_init(u8g2_rpi_hal);
-
-    //for bit bang implementations, you can use the pre-built callbacks available in the u8g2 library
-    //see https://github.com/olikraus/u8g2/wiki/Porting-to-new-MCU-platform#communication-callback-eg-u8x8_byte_hw_i2c
-    //ex: u8x8_byte_4wire_sw_spi
-    u8g2_Setup_st7920_s_128x64_f(&u8g2, U8G2_R2, cb_byte_spi_hw, cb_gpio_delay_rpi);
-
-    //Initialize Display
-    u8g2_InitDisplay(&u8g2);
-    u8g2_SetPowerSave(&u8g2, 0);
-    u8g2_ClearDisplay(&u8g2);
-
     cout << "Running display demo...Press Ctrl+C to exit" << endl;
 
     //Run display loop
     while (!complete) {
         ///Draw U8G2 Logo
-        drawU8G2Logo(&u8g2);
+        drawU8G2Logo(&screen_1);
         delay(5000);
 
         //Run Banner Animation
-        drawBannerText(&u8g2, 20);
+        drawBannerText(&screen_1, 20);
     }
 
     cout << "Exiting demo" << endl;
-    u8g2_ClearDisplay(&u8g2);
+    u8g2_ClearDisplay(&screen_1);
 }
